4c1ac84d907ba6060650a1229b1f67035ab636f0
shakthi5123 <149670275+shakthi5123@users.noreply.github.com>
2024-08-06 19:56:56 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,26 @@
+
+defaultFilters = [
+    "*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+    "*://*.trafficjunky.com/*",
+    "*://*.pornhub.com/*"
+]
+
+
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {return { cancel : true}},
+        {urls: defaultFilters},
+        ["blocking"]
+)
+
+
